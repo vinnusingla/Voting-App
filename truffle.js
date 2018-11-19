afcc363e442517a04750c293006a368dcbbc9a6e
Vinayak Singla <singla.1@iitj.ac.in>
2018-11-19 15:27:13 +0530

    Add files via upload

---
+++ truffle.js
@@ -0,0 +1,20 @@
+
+// Allows us to use ES6 in our migrations and tests.
+require('babel-register')
+
+module.exports = {
+  networks: {
+    ganache: {
+      host: '127.0.0.1',
+      port: 8545,
+      network_id: '*', // Match any network id      
+      gas: 4700000
+    },
+    development: {
+      host: '127.0.0.1',
+      port: 8545,
+      network_id: '*', // Match any network id      
+      gas: 4700000
+    }
+  }
+}
